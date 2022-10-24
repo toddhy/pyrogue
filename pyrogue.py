0f29ad8b87a22df54854fc243d6b71a267a88602
Todd <toddyosh@gmail.com>
2022-10-23 16:28:25 -1000

    generate rooms and tunnels to connect them

--- pyrogue.py
+++ pyrogue.py
@@ -24,6 +24,28 @@ fullscreen = False
 player_x = SCREEN_WIDTH // 2
 player_y = SCREEN_HEIGHT // 2
 
+ROOM_MAX_SIZE = 10 
+ROOM_MIN_SIZE = 6 
+MAX_ROOMS = 30
+
+class Rect:
+	#a rectangle on the map. use to characterize a room.
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+
+	def center(self):
+		center_x = (self.x1 + self.x2) // 2
+		center_y = (self.y1 + self.y2) // 2
+		return (center_x, center_y)
+
+	def intersect(self, other):
+		#returns true if this rectangle intersects with another one
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+			self.y1 <= other.y2 and self.y2 >= other.y1)
+			
 class Tile:
 	# a tile of the map and its properties
 	def __init__(self, blocked, block_sight = None):
@@ -58,20 +80,92 @@ class Object:
 		# erase the character that represents this object
 		tcod.console_put_char(con, self.x, self.y, ' ', tcod.BKGND_NONE)
 
+def create_room(room):
+	global map
+	# go through the tiles in the rectangle and make them passable
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			map[x][y].blocked = False
+			map[x][y].block_sight = False
+
 def make_map():
 	global map
 
-	# fill map with "unblocked" tiles
+	# fill map with "blocked" tiles
 	map = [
-		[Tile(False) for y in range(MAP_HEIGHT)]
+		[Tile(True) for y in range(MAP_HEIGHT)]
 		for x in range (MAP_WIDTH)
 	]
 
-	map[30][22].blocked = True
-	map[30][22].block_sight = True
-	map[50][22].blocked = True
-	map[50][22].block_sight = True
+	rooms = []
+	num_rooms = 0
 
+	for r in range(MAX_ROOMS):
+		#random width and height
+		w = tcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = tcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		#random position without going out of the boundaries of the map
+		x = tcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+		y = tcod.random_get_int(0, 0, MAP_HEIGHT- h - 1)
+		
+		#Rect class makes rectangles easier to work with
+		new_room = Rect(x, y, w, h)
+
+		#run through the other rooms and see if they intersect with this one
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+
+		if not failed:
+			#this means there are no intersections, so this room is valid
+
+			#"paint" it to the map's tiles
+			create_room(new_room)
+
+			#center coordinate of the new room, will be useful later
+			(new_x, new_y) = new_room.center()
+
+			if num_rooms == 0:
+				#this is the first room, where the player starts at
+				player.x = new_x
+				player.y = new_y
+
+			else:
+				#all roms after the first:
+				#connect it to the previous room with a tunnel
+
+				#center coordinate of the previous room
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+
+				#flip a coint (random number that is either 0 or 1)
+				if tcod.random_get_int(0, 0, 1) == 1:
+					#first move horizontally, then vertically
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					#first move vertically, then horizontally
+					create_v_tunnel(prev_y, new_y, new_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+
+			#finally, append the new room to the list
+			rooms.append(new_room)
+			num_rooms += 1
+
+
+def create_h_tunnel(x1, x2, y):
+	global map
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+	global map
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+		
 def render_all():
 	global color_light_wall
 	global color_light_ground
